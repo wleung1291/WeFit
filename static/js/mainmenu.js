99b1fadffab77c0a6a1762cba1314ab8df47f0ae
jjl014 <jimmyli921@gmail.com>
2016-02-05 04:07:16 +0000

    Added nav bar menu

---
+++ static/js/mainmenu.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+}
