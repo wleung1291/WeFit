02833f654243709c265464d5ea176dc449d5ac32
Wilson <w7leung@ucsd.edu>
2016-02-17 22:49:40 +0000

    removed decline button

--- static/js/dailies.js
+++ static/js/dailies.js
@@ -2,13 +2,14 @@
 function accept1(){
     var elem = document.getElementById("accept");
     if (elem.value=="Accept") elem.value = "Click to finish!";
+    $(".bye").remove(); // remove decline 
 
      // click finish will change the info of <p>
      $("#accept").click(function(){
          $('p#info').text('Congrats, +50 Leg Experience points!')
          if (elem.value=="Click to finish!"){     
               elem.value = "Done!";
-              $(".bye").remove(); // remove decline 
+              //$(".bye").remove(); // remove decline 
               $("#accept").click(function(){
                  $("#panel").slideUp("slow");
              });
@@ -18,13 +19,13 @@ function accept1(){
 function accept2(){
     var elem2 = document.getElementById("accept2");
     if (elem2.value=="Accept") elem2.value = "Click to finish!";
+    $(".bye2").remove(); // remove decline 
 
      // click finish will change the info of <p>
      $("#accept2").click(function(){
          $('p#info2').text('Congrats, +50 Leg Experience points!')
          if (elem2.value=="Click to finish!"){     
               elem2.value = "Done!";
-              $(".bye2").remove(); // remove decline 
               $("#accept2").click(function(){
                  $("#panel2").slideUp("slow");
              });
@@ -34,13 +35,13 @@ function accept2(){
 function accept3(){
     var elem3 = document.getElementById("accept3");
     if (elem3.value=="Accept") elem3.value = "Click to finish!";
+    $(".bye3").remove(); // remove decline 
 
      // click finish will change the info of <p>
      $("#accept3").click(function(){
          $('p#info3').text('Congrats, +50 Leg Experience points!')
          if (elem3.value=="Click to finish!"){     
               elem3.value = "Done!";
-              $(".bye3").remove(); // remove decline 
               $("#accept3").click(function(){
                  $("#panel3").slideUp("slow");
              });
@@ -50,13 +51,13 @@ function accept3(){
 function accept4(){
     var elem4 = document.getElementById("accept4");
     if (elem4.value=="Accept") elem4.value = "Click to finish!";
+    $(".bye4").remove(); // remove decline 
 
      // click finish will change the info of <p>
      $("#accept4").click(function(){
          $('p#info4').text('Congrats, +50 Leg Experience points!')
          if (elem4.value=="Click to finish!"){     
               elem4.value = "Done!";
-              $(".bye4").remove(); // remove decline 
               $("#accept4").click(function(){
                  $("#panel4").slideUp("slow");
              });
