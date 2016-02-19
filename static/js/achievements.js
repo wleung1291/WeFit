69cbcc6d70782013be45c3f749a155565b9a440f
Wilson <w7leung@ucsd.edu>
2016-02-19 05:04:51 +0000

    achievements

--- static/js/achievements.js
+++ static/js/achievements.js
@@ -2,14 +2,14 @@ var count = 0;
 
     function done1(){
         count = 1;
-        $('#a1').css('color','green');
+        $('#a1').css('color','black');
         $('#bye1').remove();
         var img1 = document.getElementById("icon1");
         img1.src="http://blueandgoldonline.org/wp/wp-content/uploads/2013/10/Sword.png";
         sessionStorage.done1 = true;
     }
     if (sessionStorage.done1) {
-      $('#a1').css('color','green');
+      $('#a1').css('color','black');
       var img1 = document.getElementById("icon1");
       img1.src="http://blueandgoldonline.org/wp/wp-content/uploads/2013/10/Sword.png";
       count = 1;
@@ -22,7 +22,7 @@ var count = 0;
     function done2(){
     	if(count == 1){
           count = 2;
-        	$('#a2').css('color','green');
+        	$('#a2').css('color','black');
         	$('#bye2').remove();
           var img2 = document.getElementById("icon2");
           img2.src="http://lostarmour.info/media/swords.png";
@@ -32,7 +32,7 @@ var count = 0;
     		return;}
     }
     if (sessionStorage.done2) {
-      $('#a2').css('color','green');
+      $('#a2').css('color','black');
       var img2 = document.getElementById("icon2");
       img2.src="http://lostarmour.info/media/swords.png";
       count = 2
@@ -42,7 +42,7 @@ var count = 0;
 
     function done3(){
         if(count > 2 || count == 2){
-            $('#a3').css('color','green');
+            $('#a3').css('color','black');
             $('#bye3').remove();
             var img3 = document.getElementById("icon3");
             img3.src="https://voxxeddays.com/belgrade15/wp-content/uploads/sites/8/2015/05/b-2days.png";
@@ -52,7 +52,7 @@ var count = 0;
             return;}
     }
     if (sessionStorage.done3) {
-      $('#a3').css('color','green');
+      $('#a3').css('color','black');
       var img3= document.getElementById("icon3");
       img3.src="https://voxxeddays.com/belgrade15/wp-content/uploads/sites/8/2015/05/b-2days.png";
       $('#bye3').hide();
