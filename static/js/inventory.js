976aec13af6f6e60e274844392eca26f77b95aec
Jimmy Li <Jimmy Li>
2016-02-22 23:25:50 -0800

    confirmation box thing

--- static/js/inventory.js
+++ static/js/inventory.js
@@ -8,24 +8,57 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$(".inventory-item.carrot").click(useItemCarrot);
-	$(".inventory-item.greenapple").click(useItemGreenApple);
-	$(".inventory-item.redapple").click(useItemRedApple);
-	$(".inventory-item.bread").click(useItemBread);
-	$(".inventory-item.beet").click(useItemBeet);
+//	$(".inventory-item.carrot").click(useItemCarrot);
+//	$(".inventory-item.greenapple").click(useItemGreenApple);
+//	$(".inventory-item.redapple").click(useItemRedApple);
+//	$(".inventory-item.bread").click(useItemBread);
+//	$(".inventory-item.beet").click(useItemBeet);
 
-}
-
-function useItemCarrot(){
-	if(!(document.getElementById("health").value>=100)){
-		$(".inventory-item.carrot").removeClass('inventory-item carrot');
-		if(document.getElementById("health").value >= 90){
-			document.getElementById("health").value = 100;
+	//close popup
+	$('.cd-popup').on('click', function(event){
+		if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
+			event.preventDefault();
+			$(this).removeClass('is-visible');
 		}
-		else{ document.getElementById("health").value += 10;}
-		sessionStorage.itemCarrotUsed = true;
-	}
+	});
+
+	//close popup when clicking the esc keyboard button
+	$(document).keyup(function(event){
+    	if(event.which=='27'){
+    		$('.cd-popup').removeClass('is-visible');
+	    }
+    });
 }
+
+$(".inventory-item.carrot").click(function(){
+	console.log("hm??");
+	event.preventDefault();
+	var currentHP = document.getElementById("health").value;
+	var used = false;
+	$('#popup1').addClass('is-visible');
+	console.log("._.");
+	$('#yes1').one('click', function(){
+		console.log("wut");
+		used = true;
+	});
+	$('#no1').on('click', function(){
+		console.log("the");
+		used = false;
+	});
+	if(used){
+		if(!(document.getElementById("health").value>=100)){
+			document.getElementById("health").value += 10;}
+			sessionStorage.itemCarrotUsed = true;
+		$('#popup1').removeClass('is-visible');
+		used = false;
+	}
+	else{
+		$('#popup1').removeClass('is-visible');
+		document.getElementById("health").value = currentHP;
+		used = false;
+	}
+	console.log("end");
+});
 if(sessionStorage.itemCarrotUsed){
 	console.log("Carrot Eaten");
 	$(".inventory-item.carrot").removeClass('inventory-item carrot');	
@@ -35,17 +68,22 @@ if(sessionStorage.itemCarrotUsed){
 	else{ document.getElementById("health").value += 10;}
 }
 
-
-function useItemGreenApple(){
-	if(!(document.getElementById("health").value>=100)){
-		$(".inventory-item.greenapple").removeClass('inventory-item greenapple');
-		if(document.getElementById("health").value >= 90){
-			document.getElementById("health").value = 100;
-		}
-		else{ document.getElementById("health").value += 10;}
-		sessionStorage.itemGreenAppleUsed = true;	
-	}
-}
+$(".inventory-item.greenapple").click(function(){
+	event.preventDefault();
+	var currentHP = document.getElementById("health").value;
+	$('#popup2').addClass('is-visible');
+	$('#yes2').click(function(){
+			if(!(document.getElementById("health").value>=100)){
+				document.getElementById("health").value += 10;
+				sessionStorage.itemGreenAppleUsed = true;	
+			}
+			$('#popup2').removeClass('is-visible');
+	});
+	$('#no2').on('click', function(){
+		$('#popup2').removeClass('is-visible');
+		document.getElementById("health").value = currentHP;
+	});
+});
 if(sessionStorage.itemGreenAppleUsed){
 	console.log("Green Apple Eaten");
 	$(".inventory-item.greenapple").removeClass('inventory-item greenapple');	
@@ -55,16 +93,23 @@ if(sessionStorage.itemGreenAppleUsed){
 	else{ document.getElementById("health").value += 10;}
 }
 
-function useItemRedApple(){
-	if(!(document.getElementById("health").value>=100)){
-		$(".inventory-item.redapple").removeClass('inventory-item redapple');
-		if(document.getElementById("health").value >= 90){
-			document.getElementById("health").value = 100;
-		}
-		else{ document.getElementById("health").value += 10;}
-		sessionStorage.itemRedAppleUsed = true;
-	}
-}
+$(".inventory-item.redapple").click(function(){
+	event.preventDefault();
+	var currentHP = document.getElementById("health").value;
+	$('#popup3').addClass('is-visible');
+	$('#yes3').click(function(){
+			if(!(document.getElementById("health").value>=100)){
+				$(".inventory-item.redapple").removeClass('inventory-item redapple');
+				document.getElementById("health").value += 10;
+				sessionStorage.itemRedAppleUsed = true;
+			}
+			$('#popup3').removeClass('is-visible');
+	});
+	$('#no3').on('click', function(){
+		$('#popup3').removeClass('is-visible');
+		document.getElementById("health").value = currentHP;
+	});
+});
 if(sessionStorage.itemRedAppleUsed){
 	console.log("Red Apple Eaten");
 	$(".inventory-item.redapple").removeClass('inventory-item redapple');	
@@ -74,38 +119,49 @@ if(sessionStorage.itemRedAppleUsed){
 	else{ document.getElementById("health").value += 10;}
 }
 
-function useItemBread(){
-	if(!(document.getElementById("health").value>=100)){
-		$(".inventory-item.bread").removeClass('inventory-item bread');
-		if(document.getElementById("health").value >= 90){
-			document.getElementById("health").value = 100;
-		}
-		else{ document.getElementById("health").value += 10;}
-		sessionStorage.itemBreadUsed = true;
-	}	
-}
+$(".inventory-item.bread").click(function(){
+	event.preventDefault();
+	var currentHP = document.getElementById("health").value;
+	$('#popup4').addClass('is-visible');
+	$('#yes4').click(function(){
+			if(!(document.getElementById("health").value>=100)){
+				$(".inventory-item.bread").removeClass('inventory-item bread');
+				document.getElementById("health").value += 10;
+				sessionStorage.itemBreadUsed = true;
+			}	
+			$('#popup4').removeClass('is-visible');	
+	});
+	$('#no4').on('click', function(){
+		$('#popup4').removeClass('is-visible');
+		document.getElementById("health").value = currentHP;
+	});
+});
 if(sessionStorage.itemBreadUsed){
 	console.log("Bread Eaten");
 	$(".inventory-item.bread").removeClass('inventory-item bread');	
-	if(document.getElementById("health").value >= 90){
-		document.getElementById("health").value = 100;
-	}
-	else{ document.getElementById("health").value += 10;}
+	document.getElementById("health").value += 10;
 }
 
-function useItemBeet(){
-	if(!(document.getElementById("health").value>=100)){
-		$(".inventory-item.beet").hide();
-		if(document.getElementById("health").value >= 90){
-			document.getElementById("health").value = 100;
-		}
-		else{ document.getElementById("health").value += 10;}
-		sessionStorage.itemBeetUsed = true;
-	}
-}
+$(".inventory-item.beet").click(function(){
+	event.preventDefault();
+	var currentHP = document.getElementById("health").value;
+	$('#popup5').addClass('is-visible');
+	$('#yes5').click(function(){
+			if(!(document.getElementById("health").value>=100)){
+				$(".inventory-item.beet").removeClass('inventory-item beet');
+				document.getElementById("health").value += 10;
+				sessionStorage.itemBeetUsed = true;
+			}
+			$('#popup5').removeClass('is-visible');
+	});
+	$('#no5').on('click', function(){
+		$('#popup5').removeClass('is-visible');
+		document.getElementById("health").value = currentHP;
+	});
+});
 if(sessionStorage.itemBeetUsed){
 	console.log("Beet Eaten");
-	$(".inventory-item.beet").hide();
+	$(".inventory-item.beet").removeClass('inventory-item beet');
 	if(document.getElementById("health").value >= 90){
 		document.getElementById("health").value = 100;
 	}
